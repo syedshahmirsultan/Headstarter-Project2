5d24442845bfc902166d5012bd8832b1308d659e
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-08-03 10:39:05 +0500

    updated code

--- app/Pantry/page.tsx
+++ app/Pantry/page.tsx
@@ -2,6 +2,7 @@ import { getKindeServerSession, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/
 import AddDialog from "../Components/AddDialog";
 import TaskList from "../Components/TaskList";
 import  Link  from "next/link";
+import { IoIosArrowBack } from "react-icons/io";
 
 
 export default async function Pantry() {
@@ -12,7 +13,7 @@ export default async function Pantry() {
     { user ?  (
     <LogoutLink className="bg-black hover:bg-gray-700 fixed right-2 md:right-10 top-2 md:top-10 text-md font-semibold text-white h-12 w-24 flex justify-center items-center  border border-white rounded-md">Log out</LogoutLink>
      ):(
-        <Link href="/" className="bg-black hover:bg-gray-700  text-md font-semibold text-white h-12 w-24 flex justify-center items-center  border border-white rounded-md">Home</Link>
+        <Link href="/"  className="bg-blue-700 hover:bg-blue-500 fixed right-2 md:right-10 top-2 md:top-10 text-md font-semibold text-white h-12 w-24 flex justify-center items-center  border border-white rounded-md"> <IoIosArrowBack size={18} color={'white'} className="mr-2"/> Home</Link>
 )} <div className="w-full p-2 flex flex-col items-center md:p-0 mt-20 md:mt-24 md:max-w-6xl h-full md:mx-auto">
     <h1 className="text-3xl md:text-5xl text-center font-extrabold text-gray-950 tracking-wide"> Shahmir Pantry Tracker</h1>
     <AddDialog/>
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ const Home = async() => {
       user?.id ?
        (
         <Link href="/Pantry"
-         className='px-5 py-2.5 mt-12 bg-white rounded-md flex justify-center items-center text-black font-semibold text-md hover:bg-gray-200'>
+         className='px-6 py-3 mt-12 bg-blue-700 hover:bg-blue-600 rounded-md flex justify-center items-center text-white font-semibold text-md '>
          Get Started </Link>) :
           (<div className='flex gap-x-4 justify-center mt-12'><LoginLink className='px-5 py-2.5 bg-white rounded-md flex justify-center items-center text-black hover:bg-gray-200 font-semibold text-md'>Sign in</LoginLink>
 <RegisterLink className='px-5 py-2.5 bg-white rounded-md flex justify-center items-center text-black font-semibold text-md hover:bg-gray-200'>Sign up</RegisterLink>
